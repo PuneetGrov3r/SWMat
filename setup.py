04e7c5de4950f36fe73d61475f62850ffbc17fa1
Puneet Grover <grover.puneet1995@gmail.com>
2019-03-30 02:30:14 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'SWMat',
   packages = ['SWMat'],
-  version = '0.1.1',
+  version = '0.1.2',
   license='Apache License 2.0',
   description = 'A package for making stunning graphs/charts using matplotlib in just few lines of code!',
   author = 'Puneet Grover',
   author_email = 'grover.puneet1995@gmail.com',
   url = 'https://github.com/PuneetGrov3r/SWMat',
-  download_url = 'https://github.com/PuneetGrov3r/SWMat/archive/v0.1.1-alpha.tar.gz',
+  download_url = 'https://github.com/PuneetGrov3r/SWMat/archive/v0.1.2-alpha.tar.gz',
   keywords = ['plot', 'visualization', 'data', 'big data', 'exploration', 'data exploration', 'communication', 'python', 'matplotlib', 'machine learning', 'data science'],
   install_requires=[
           'matplotlib',
